613fd4e3824ddbfc9e938ad0f6203c9605f02efe
Sarrveshhh <sarrvesharyaa@gmail.com>
2024-01-31 12:41:56 +0530

    tailwind css config added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css';
 import {Home} from "./Components/Home";
 import {MeetList} from "./Components/MeetList";
 import {Setmeet} from "./Components/Setmeet";
--- client/src/components/Setmeet.jsx
+++ client/src/components/Setmeet.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 
 export const Setmeet = () => {
   return (
-    <div>Setmeet</div>
+    <div className="setmeet">
+        <h1 className="">Create Meet Link</h1>
+
+    </div>
   )
 }
