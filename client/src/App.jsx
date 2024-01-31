7376ce6bce396282985ccbea67f6d2df106dd3c3
unknown <md.r.a.n.786@gmail.com>
2024-01-31 12:16:38 +0530

    created components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,22 @@
 import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {Home} from "Components/Home";
+import {MeetList} from "Components/MeetList";
+import {Setmeet} from "Components/Setmeet";
+
 
 function App() {
   return (
     <div className='main-body'>
-      
+        <NoteState>
+        <Router>
+            <Routes>
+              <Route exact path="/" element={<Home />} />
+              <Route exact path="/meetlist" element={<MeetList/>} />
+              <Route exact path="/setmeet" element={<Setmeet/>} />
+            </Routes>
+        </Router>
+      </NoteState>
     </div>
   )
 }
