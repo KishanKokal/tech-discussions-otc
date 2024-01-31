294d254af8e4188933da2aa2fef9da1df9e169fb
unknown <md.r.a.n.786@gmail.com>
2024-01-31 23:33:06 +0530

    added post req

--- client/src/components/Setmeet.jsx
+++ client/src/components/Setmeet.jsx
@@ -1,22 +1,59 @@
 import { useState, useRef, useEffect } from 'react';
 import '../App1.css';
+import axios from 'axios';
 
 export const Setmeet = () => {
+  const [formData, setFormData] = useState({
+    title: '',
+    description: '',
+    meetdate: '',
+    time: '',
+    link: '',
+  });
+  const [meetCreated, setMeetCreated] = useState(false);
+  const formRef = useRef(null);
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log(formData)
+    try {
+      const response = await axios.post('http://localhost:3000/upcomingEvents', formData);
+      setMeetCreated(true);
+        setFormData({
+          title: '',
+          description: '',
+          meetdate: '',
+          time: '',
+          link: '',
+        });
+        formRef.current.reset();
+        console.log("data sent")
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
   return (
-    <div className="home-div">
+    <div className="home-div pt-16">
       <div className="content-left">
         <h2 className="content-header">Creat Meet Link</h2>
         <div className="content-para">Please fill all the necessary details. You can then find this event in upcoming list.</div>
       </div>
-      <div className="form-right">
-        <input type="text" className="input-fname inp" placeholder="Title"/>
-        <input type="text" className="input-lname inp" placeholder="Description" />
-        <input type="date" className="input-email inp pr-5" placeholder="Date" />
-        <input type="time" className="input-email inp pr-5" placeholder="Date" />
-        <input type="text" className="input-roomname inp" placeholder="Meet Link" />
-        <button className="input-btn">Click to create Meet</button>
-      </div>
-      
+      <form className="form-right" ref={formRef}>
+        <input type="text" className="input-fname inp" placeholder="Title" name="title" onChange={handleChange}/>
+        <input type="text" className="input-lname inp" placeholder="Description" name="description" onChange={handleChange} />
+        <input type="date" className="input-email inp pr-5" placeholder="Date" name="meetdate" onChange={handleChange}/>
+        <input type="time" className="input-email inp pr-5" placeholder="Date" name="time" onChange={handleChange}/>
+        <input type="text" className="input-roomname inp" placeholder="Meet Link" name="link" onChange={handleChange}/>
+        <button className="input-btn" onClick={handleSubmit}>Click to create Meet</button>
+        {meetCreated && <div className="meet-created-message text-center text-lime-800">Meet Created</div>}
+      </form>
     </div>
   );
 }
