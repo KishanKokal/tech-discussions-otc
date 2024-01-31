8b736c045dc9774cb31896eb7f2a150760d6f093
Kishan Kokal <kokalkishan.official@gmail.com>
2024-01-31 13:54:04 +0530

    feat: video-upload and video-processing

--- server/src/resources/video/video.controllers.js
+++ server/src/resources/video/video.controllers.js
@@ -1,8 +1,16 @@
+import { convertSpeechToText } from "../utils/video.js";
+import { summarizeTranscript } from "../utils/summarize.js";
 /**
  *
  * @param {*} req
  * @param {*} res
  */
-export const uploadVideo = (req, res) => {
-  res.json({ message: "upload" });
+export const uploadVideo = async (req, res) => {
+  res.status(200).json({ message: "uploaded successfully" });
+  console.log("uploaded successfully, calling convertSpeechToText");
+  let response = await convertSpeechToText(req.file.filename);
+  let text = response.text;
+  console.log("speech converted to text, next calling summarizeTranscript");
+  let summary = await summarizeTranscript(text);
+  console.log(summary);
 };
